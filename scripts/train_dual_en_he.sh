9345c7bcd53f38863d2d4569b3bb93a9baa03fc9
Elad Hoffer <elad.hoffer@gmail.com>
2017-07-26 11:30:56 +0300

    updated english-hebrew script

--- scripts/train_dual_en_he.sh
+++ scripts/train_dual_en_he.sh
@@ -8,9 +8,15 @@ python main.py \
   --dataset_dir ${DATASET_DIR} \
   --results_dir ${OUTPUT_DIR} \
   --model RecurrentAttentionSeq2Seq \
-  --model_config "{'num_layers': 2, 'hidden_size': 256, 'tie_embedding': True, 'dropout': 0.2}" \
-  --data_config "{'languages': ['he','en'], 'mark_language': True, 'tokenization':'bpe', 'num_symbols':32000, 'shared_vocab':True}" \
+  --model_config "{'hidden_size': 512, 'dropout': 0.2, \
+                   'tie_embedding': True, 'transfer_hidden': False, \
+                   'encoder': {'num_layers': 3, 'bidirectional': True, 'num_bidirectional': 1}, \
+                   'decoder': {'num_layers': 4,  'num_pre_attention_layers': 1, 'context_transform': 512, \
+                               'attention': {'mode': 'bahdanau', 'normalize': True}}}" \
+  --data_config "{'languages': ['he','en'], 'mark_language': True, 'tokenization':'bpe',\
+                  'num_symbols':32000, 'shared_vocab':True}" \
   --b 64 \
+  --max_length 80 \
   --trainer MultiSeq2SeqTrainer \
   --optimization_config "{0: {'optimizer': 'Adam', 'lr': 1e-3},
                           4: {'optimizer': 'Adam', 'lr': 1e-4},
