ef6605040343a855e38c81e3c0ee8bcab652c819
Elad Hoffer <ehoffer@habana.ai>
2018-08-02 14:14:16 +0300

    update

--- seq2seq/tools/trainer.py
+++ seq2seq/tools/trainer.py
@@ -72,7 +72,7 @@ class Seq2SeqTrainer(object):
         super(Seq2SeqTrainer, self).__init__()
         self.model = model
         self.criterion = criterion or CrossEntropyLoss(
-            size_average=False, ignore_index=PAD, smooth_eps=label_smoothing)
+            ignore_index=PAD, smooth_eps=label_smoothing, reduction='sum')
 
         self.optimizer = OptimRegime(self.model.parameters(), regime=regime)
         self.grad_clip = grad_clip
