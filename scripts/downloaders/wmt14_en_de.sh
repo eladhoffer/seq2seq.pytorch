51cdd6cc16c20038d8d6c13561774c302e10f77c
Elad Hoffer <elad.hoffer@gmail.com>
2019-02-03 13:58:17 +0200

    Update wmt14_en_de.sh

--- scripts/downloaders/wmt14_en_de.sh
+++ scripts/downloaders/wmt14_en_de.sh
@@ -113,7 +113,7 @@ done
 for f in ${OUTPUT_DIR}/*.en; do
   fbase=${f%.*}
   echo "Cleaning ${fbase}..."
-  ${OUTPUT_DIR}/mosesdecoder/scripts/training/clean-corpus-n.perl $fbase de en "${fbase}.clean" 1 100
+  ${OUTPUT_DIR}/mosesdecoder/scripts/training/clean-corpus-n.perl -ratio 1.5 $fbase de en "${fbase}.clean" 1 100
 done
 
 
