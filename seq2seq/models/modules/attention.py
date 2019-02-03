cb5e4f8b916a17fcc6440fbfbe3f4f6e11bed3ca
Elad Hoffer <elad.hoffer@gmail.com>
2019-02-03 15:39:10 +0200

    Update attention.py

--- seq2seq/models/modules/attention.py
+++ seq2seq/models/modules/attention.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 from .weight_norm import weight_norm as wn
 
 """ Implementations of attention layers."""
